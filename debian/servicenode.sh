98b9b1c3cf3d73cfc28152e724f12427925222a1
Ethan van Ballegooyen <ethanvb55@gmail.com>
2019-08-30 17:06:07 +0200

    Added SN Scripts, Scripts will deploy IPFS on all node tiers

--- debian/servicenode.sh
+++ debian/servicenode.sh
@@ -1,44 +1,23 @@
 #!/usr/bin/env sh
-_user=${SUDO_USER:-$(whoami)}
-_upgrade="No"
-_nodetype="masternode"
-
-for opt in "$@"
-do
-  if [ $opt = "-masternode" ] ; then
-    _nodetype="masternode"
-  elif [ $opt = "-gatewaynode" ] ; then
-    _nodetype="gatewaynode"
-  elif [ $opt = "-upgrade" ] ; then
-    _upgrade="Yes"
-  else
-    echo "Invalid option: $opt"
-  fi
-done
-
-if [ $_nodetype = "gatewaynode" ] ; then
-  echo "ethoFS Gateway Node Setup Initiated"
-fi
-if [ $_nodetype = "masternode" ] ; then
-  echo "Ether-1 Masternode Setup Initiated"
-fi
-
-if [ $_upgrade = "Yes" ] ; then
-  echo "Upgrade Option Selected"
-fi
+_user="$(id -u -n)"
 
 echo '**************************'
 echo 'Installing misc dependencies'
 echo '**************************'
+
 # install dependencies
-sudo yum install unzip wget -y
+sudo apt-get update && sudo apt-get install systemd unzip wget -y
 
 echo '**************************'
 echo 'Installing Ether-1 Node binary'
 echo '**************************'
-# Download node binary
 
-wget https://github.com/Ether1Project/Ether-1-GN-Binaries/releases/download/1.2.1/Ether1-MN-SN-1.2.1.tar.gz
+# Download node binary
+sudo systemctl stop ether1node
+
+sudo rm geth
+
+wget https://github.com/Ether1Project/Ether-1-SN-MN-Binaries/releases/download/1.2.1/Ether1-MN-SN-1.2.1.tar.gz
 
 tar -xzf Ether1-MN-SN-1.2.1.tar.gz
 
@@ -49,12 +28,12 @@ chmod +x geth
 rm Ether1-MN-SN-1.2.1.tar.gz
 
 echo '**************************'
-echo 'Creating and setting up' $_nodetype 'Node system service'
+echo 'Creating and setting up system service'
 echo '**************************'
 
 cat > /tmp/ether1node.service << EOL
 [Unit]
-Description=Ether1 Gateway/Master Node
+Description=Ether1 Masternode/Service Node
 After=network.target
 
 [Service]
@@ -65,25 +44,23 @@ Group=$_user
 Type=simple
 Restart=always
 
-ExecStart=/usr/sbin/geth --syncmode=fast --cache=512 --datadir=/home/$_user/.ether1 --lightpeers 100 --lightserv 40
+ExecStart=/usr/sbin/geth --syncmode=fast --cache=512
 
 [Install]
 WantedBy=default.target
 EOL
 
-sudo systemctl stop ether1node
 sudo \mv /tmp/ether1node.service /etc/systemd/system
+sudo \rm /usr/sbin/geth
 sudo \mv geth /usr/sbin/
-sudo systemctl daemon-reload
-sudo systemctl enable ether1node && systemctl start ether1node
-sudo systemctl restart ether1node
-sudo systemctl status ether1node --no-pager --full
+sudo systemctl enable ether1node && sudo systemctl stop ether1node && sudo systemctl start ether1node
+systemctl status ether1node --no-pager --full
 
 echo '**************************'
 echo 'Node Setup Complete....Deploying IPFS'
 echo '**************************'
 
-cd /home/$_user
+cd $HOME
 wget https://github.com/Ether1Project/Ether-1-GN-Binaries/releases/download/1.2.1/ipfs.tar.gz
 tar -xzf ipfs.tar.gz
 chmod +x ipfs
@@ -107,7 +84,7 @@ Group=$_user
 Type=simple
 Restart=always
 
-ExecStart=/usr/sbin/ipfs daemon --migrate --enable-namesys-pubsub --enable-gc
+ExecStart=/usr/sbin/ipfs daemon --migrate --enable-namesys-pubsub  --enable-gc --routing=dhtclient
 
 [Install]
 WantedBy=default.target
@@ -116,23 +93,10 @@ EOL
 sudo systemctl stop ipfs
 sudo \mv /tmp/ipfs.service /etc/systemd/system
 sudo \mv ipfs /usr/sbin/
-
-if [ $_upgrade = "No" ] ; then
-  sudo rm -r $HOME/.ipfs
-  sudo rm -r /home/$_user/.ipfs
-  ipfs init
-fi
+sudo rm -r $HOME/.ipfs
+ipfs init
 ipfs bootstrap rm --all
-if [ $_nodetype = "gatewaynode" ] ; then
-  _maxstorage="78GB"
-  sudo setcap CAP_NET_BIND_SERVICE=+eip /usr/sbin/ipfs
-  ipfs config Addresses.Gateway /ip4/0.0.0.0/tcp/80
-fi
-if [ $_nodetype = "masternode" ] ; then
-  _maxstorage="38GB"
-  sudo setcap CAP_NET_BIND_SERVICE=+eip /usr/sbin/ipfs
-  ipfs config Addresses.Gateway /ip4/0.0.0.0/tcp/80
-fi
+_maxstorage="18GB"
 
 ipfs config Datastore.StorageMax $_maxstorage
 ipfs config --json Swarm.ConnMgr.LowWater 400
@@ -147,8 +111,7 @@ ipfs bootstrap add /ip4/45.77.170.137/tcp/4001/ipfs/QmTZsBNb7dfJJmwuAdXBjKZ7ZH6X
 ipfs bootstrap add /ip4/51.38.131.241/tcp/4001/ipfs/Qmf4oLLYAhkXv95ucVvUihnWPR66Knqzt9ee3CU6UoJKVu
 ipfs bootstrap add /ip4/51.77.150.202/tcp/4001/ipfs/QmUEy4ScCYCgP6GRfVgrLDqXfLXnUUh4eKaS1fDgaCoGQJ
 ipfs bootstrap add /ip4/142.44.246.43/tcp/4001/ipfs/QmPW8zExrEeno85Us3H1bk68rBo7N7WEhdpU9pC9wjQxgu
-sudo mv $HOME/.ipfs /home/$_user/
-sudo chown -R $_user:$_user /home/$_user/.ipfs
+sudo chown -R $_user:$_user $HOME/.ipfs
 
 cat > /tmp/swarm.key << EOL
 /key/swarm/psk/1.0.0/
@@ -156,7 +119,7 @@ cat > /tmp/swarm.key << EOL
 38307a74b2176d0054ffa2864e31ee22d0fc6c3266dd856f6d41bddf14e2ad63
 EOL
 
-sudo \mv /tmp/swarm.key /home/$_user/.ipfs
+sudo \mv /tmp/swarm.key $HOME/.ipfs
 sudo systemctl daemon-reload
 sudo systemctl enable ipfs && systemctl start ipfs
 sudo systemctl restart ipfs
@@ -166,7 +129,7 @@ echo '**************************'
 echo 'IPFS Setup Complete....Deploying ethoFS'
 echo '**************************'
 
-cd /home/$_user
+cd $HOME
 wget https://github.com/Ether1Project/Ether-1-GN-Binaries/releases/download/1.2.1/ethoFS.tar.gz
 tar -xzf ethoFS.tar.gz
 chmod +x ethoFS
@@ -190,7 +153,7 @@ Group=$_user
 Type=simple
 Restart=always
 
-ExecStart=/usr/sbin/ethoFS -$_nodetype
+ExecStart=/usr/sbin/ethoFS -servicenode
 
 [Install]
 WantedBy=default.target
@@ -207,3 +170,5 @@ sudo systemctl status ethoFS --no-pager --full
 echo '**************************'
 echo 'ethoFS Setup Complete'
 echo '**************************'
+
+echo 'Done.'
